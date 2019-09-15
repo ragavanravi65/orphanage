41eeb973a6218e73f7c3b73b2ca1bcae14bd0616
ragav <ragavkutti777@gmail.com>
2019-09-15 22:37:06 +0530

    changed to firebase

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express=require("express");
 var app=express();
 var bodyParser=require("body-parser");
-var nodemailer=require("nodemailer");
-
+// var nodemailer=require("nodemailer");
+ 
 
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
@@ -24,42 +24,45 @@ app.get("/news",function(req,res){
 });
 
 //contact form===================================================================================
-app.post("/sendmail",function(req,res){
-    var output=`<p>You have a new contact enrollment candidate</p>
-    <h3>Contact Details</h3>
-    <ul style="color:#5e038c;font-size:16px;">
-        <li><strong>NAME:</strong>${req.body.firstname}</li>
-        <li><strong>E-MAIL:</strong>${req.body.email}</li>
-        <li><strong>PHONE:</strong>${req.body.phone}</li>
-        <h4>Message</h4>
-        <h3 >${req.body.textarea}</h3>
-    </ul>`
+// app.post("/sendmail",function(req,res){
+//     var output=`<p>You have a new contact enrollment candidate</p>
+//     <h3>Contact Details</h3>
+//     <ul style="color:#5e038c;font-size:16px;">
+//         <li><strong>NAME:</strong>${req.body.firstname}</li>
+//         <li><strong>E-MAIL:</strong>${req.body.email}</li>
+//         <li><strong>PHONE:</strong>${req.body.phone}</li>
+//         <h4>Message</h4>
+//         <h3 >${req.body.textarea}</h3>
+//     </ul>`
 //node to gmail account mail processing==================================================================
-var transporter=nodemailer.createTransport({
-    service:'gmail' ,
-    auth:{
-        user:'ragavkutti777@gmail.com' ,
-        pass:'ragavkutti777'
-    }
-});
+// var transporter=nodemailer.createTransport({
+//     service:'gmail' ,
+//     auth:{
+//         user:'ragavkutti777@gmail.com' ,
+//         pass:'ragavkutti777'
+//     }
+// });
 
-var mailOptions={
-    from:'"Donation Contact Portal"<ragavkutti777@gmail.com>',
+// var mailOptions={
+//     from:'"Donation Contact Portal"<ragavkutti777@gmail.com>',
 
-    to:'ragavanravi65@gmail.com',
-    subject:'Saravana contact portal',
-    html:output
-};
+//     to:'ragavanravi65@gmail.com',
+//     subject:'Saravana contact portal',
+//     html:output
+// };
 
 
-transporter.sendMail(mailOptions,function(err,info){
-    if(err){
-        console.log(err);
-    }else{
-        console.log("mail sent"+info.response);
-    }
-});
+// transporter.sendMail(mailOptions,function(err,info){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("mail sent"+info.response);
+//     }
+// });
 
+// res.redirect("/#contactdrive");
+// });
+app.post("/send",function(req,res){
 res.redirect("/#contactdrive");
 });
 //==========================================================================
--- public/stylesheets/js/formval.js
+++ public/stylesheets/js/formval.js
@@ -1,33 +1,77 @@
+//print error
+function printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg;
+   
+}
 //form validation
 function Validation(){
 	var fname=document.getElementById("fname").value;
 	var email=document.getElementById("email").value;
 	var phone=document.getElementById("phone").value;
 	var message=document.getElementById("textarea").value;
-	var errormessage=document.getElementById("errormessage");
-
-    errormessage.style.padding="5px";
-    if(fname.length<5){
-        text="Please enter valid name";
-        errormessage.innerHTML=text;
-        return false;
+    // var errormessage=document.getElementById("errormessage");
+ 
+    // errormessage.style.padding="5px";
+    // if((fname.length<5) || (email.indexOf("@")==-1 || email.length<5) || (isNaN(phone) || phone.length!=10) || (message.length<5)){
+    //     text="Please provide us proper credentials"
+    //     errormessage.innerHTML=text;
+    //     count=0;
+    //     return false;
+    // }
+    if(fname.length<2){
+        printError("nameerr","Please enter a valid name(min-length:2)*");   
+        fname=false;    
+    }else{
+        printError("nameerr","");
     }
+
     if(email.indexOf("@")==-1 || email.length<5){
-        text="Invalid E-mail";
-        errormessage.innerHTML=text;
-        return false;
+        printError("emailerr","Invalid E-mail*");
+        email=false;
+    }else{
+        printError("emailerr","");
     }
     if(isNaN(phone) || phone.length!=10){
-        text="Incorrect Mobile Number";
-        errormessage.innerHTML=text;
-        return false;
+        printError("phoneerr","Enter 10 digit mobile number*");
+        phone=false;
+    }else{
+        printError("phoneerr","");
     }
     if(message.length<5){
-        text="Enter a Valid Message";
-        errormessage.innerHTML=text;
+        printError("messageerr","Enter a Valid Message more than 5 letters*");
+        message=false;
+    }else{
+        printError("messageerr","");
+    }
+
+    if((fname && email && phone && message) == false){
         return false;
+    }
+    else{
+        //reference messages collections
+        
+        
+        function saveMessage(fname,email,phone,message){
+            var newMessageRef=messageRef.push();
+                newMessageRef.set({
+                Fullname:fname,
+                Email:email,
+                Mobile:phone,
+                Message:message
+            });
+        }
+            saveMessage(fname,email,phone,message);
+            //show alert
+            document.querySelector('.alert').style.display='block';
+            // document.querySelector('#errormessage').style.display='none';
+            ///hiding alert after 2sec
+            setTimeout(function(){
+                document.querySelector('.alert').style.display='none';
+            },3000);
+            //remove old content
+            document.getElementById('contactform').reset();
+            
     }
     
     return true;
-  
 }
